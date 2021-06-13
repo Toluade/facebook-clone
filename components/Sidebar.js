1a2a02542722549b079865e006762eaab29ebcde
Tolu Adeoye <toluade1512@gmail.com>
2021-06-13 21:49:08 +0100

    Build Sidebar component

---
+++ components/Sidebar.js
@@ -0,0 +1,32 @@
+import { useSession } from "next-auth/client";
+import {
+  ChevronDownIcon,
+  ShoppingBagIcon,
+  UserGroupIcon,
+} from "@heroicons/react/outline";
+import {
+  CalendarIcon,
+  ClockIcon,
+  DesktopComputerIcon,
+  UsersIcon,
+} from "@heroicons/react/solid";
+import SidebarRow from "./SidebarRow";
+
+function Sidebar() {
+  const [session] = useSession();
+
+  return (
+    <div className="p-2 mt-5 max-w-[600px] xl:min-w-[300px]">
+      <SidebarRow src={session.user.image} title={session.user.name} />
+      <SidebarRow Icon={UsersIcon} title="Friends" />
+      <SidebarRow Icon={UserGroupIcon} title="Groups" />
+      <SidebarRow Icon={ShoppingBagIcon} title="Marketplace" />
+      <SidebarRow Icon={DesktopComputerIcon} title="Watch" />
+      <SidebarRow Icon={CalendarIcon} title="Events" />
+      <SidebarRow Icon={ClockIcon} title="Memories" />
+      <SidebarRow Icon={ChevronDownIcon} title="See More" />
+    </div>
+  );
+}
+
+export default Sidebar;
